b15be1b34fd75d7dd03e64cf6fba12a9017a2938
= <wjdrlans4000@naver.com>
2021-07-20 00:06:38 +0900

    test : generate 메서드  테스트코드 작성

---
+++ STEP_02/test/me/gimun/LottoGenerateTest.java
@@ -0,0 +1,21 @@
+package me.gimun;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoGenerateTest {
+
+    @Test
+    void generate() {
+        LottoGenerate lottoGenerate = new LottoGenerate();
+
+        lottoGenerate.generate();
+        List<Integer> lotto = lottoGenerate.getLotto();
+        int bonus = lottoGenerate.getBonusNum();
+        System.out.println(lotto);
+        System.out.println(bonus);
+    }
+}
