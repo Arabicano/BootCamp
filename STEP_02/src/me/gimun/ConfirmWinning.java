86c2d95be1d44c4bf03a2572623d035d040076cb
= <wjdrlans4000@naver.com>
2021-07-24 00:09:18 +0900

    feat : 당첨확인 기능 개발

--- STEP_02/src/me/gimun/ConfirmWinning.java
+++ STEP_02/src/me/gimun/ConfirmWinning.java
@@ -1,4 +1,56 @@
 package me.gimun;
 
+import java.util.List;
+
 public class ConfirmWinning {
+    /*
+    * 당첨 여부 확인
+    * 당첨 종류
+    * 1등 : 6개일치
+    * 2등 : 5개 / 보너스 일치
+    * 3등 : 4개 일치
+    * 4등 : 3개 일치
+    * 나머지 낙첨
+    * */
+
+    public String confirmLotto(List<Integer> winningLotto, int bonus, List<Integer> buyLotto){
+        String result = "";
+        int winNumbers = 0;
+        boolean isBonus = false;
+        for (int i = 0; i < buyLotto.size(); i++) {
+            for (int j = 0; j < winningLotto.size(); j++) {
+                if (buyLotto.get(i) == winningLotto.get(j)) {
+                    winNumbers++;
+                }
+                else if(buyLotto.get(i) != winningLotto.get(j)
+                            && buyLotto.get(i) == bonus){
+                    isBonus = true;
+                }
+            }
+        }
+        switch (winNumbers){
+            case 6:
+                result = "1등";
+                break;
+            case 5:
+                if(isBonus){
+                    result = "2등";
+                }else{
+                    result = "낙첨";
+                }
+                break;
+            case 4:
+                result = "3등";
+                break;
+            case 3:
+                result = "4등";
+                break;
+            default:
+                result = "낙첨";
+                break;
+        }
+
+        return result;
+    }
+
 }
---
+++ STEP_02/test/me/gimun/ConfirmWinningTest.java
@@ -0,0 +1,44 @@
+package me.gimun;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConfirmWinningTest {
+
+    @Test
+    void confirmLotto() {
+        //로또생성
+        LottoGenerate lottoGenerate = new LottoGenerate();
+        lottoGenerate.generate();
+        List<Integer> lotto = lottoGenerate.getLotto();
+
+        //당첨번호 지정
+        SettingWinningNumber settingWinningNumber = new SettingWinningNumber();
+        Set<Integer> set = new HashSet<>();
+        set.add(3);
+        set.add(10);
+        set.add(13);
+        set.add(21);
+        set.add(26);
+        set.add(31);
+
+        List<Integer> winningLotto = new ArrayList<Integer>();
+        winningLotto.addAll(set);
+        Collections.sort(winningLotto);
+
+        //보너스번호
+        winningLotto.add(45);
+        settingWinningNumber.winningNumSet(winningLotto);
+
+        //당첨확인
+        ConfirmWinning confirmWinning = new ConfirmWinning();
+        String result = confirmWinning.confirmLotto(settingWinningNumber.getWinningLotto(),settingWinningNumber.getWinningBonusNum(),lotto);
+
+        System.out.println("내가 구매한 로또 번호 : " + lotto);
+        System.out.println("당첨번호 : " + settingWinningNumber.getWinningLotto() + " 보너스번호 : " + settingWinningNumber.getWinningBonusNum());
+        System.out.println("당첨확인 : " + result);
+    }
+}
