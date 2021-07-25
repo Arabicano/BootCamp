423a9b0d89d0dd798c5c92ea659653a5c4c92734
sjchoi <allure1212.c@gmail.com>
2021-07-25 23:10:15 +0900

    #[maven] Junit4 에서 Junit5 로 버전 변경

--- STEP_02/lotto/src/test/java/com/bootcamp/AppTest.java
+++ STEP_02/lotto/src/test/java/com/bootcamp/AppTest.java
@@ -1,7 +1,7 @@
 package com.bootcamp;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -11,11 +11,8 @@ import java.io.PrintStream;
 
 import com.bootcamp.lotto.service.LottoService;
 
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import jdk.jfr.Timestamp;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
@@ -24,7 +21,7 @@ public class AppTest {
 
     private static LottoService lottoService;
 
-    @BeforeClass
+    @BeforeEach
     static void initAll() {
         lottoService = new LottoService();
     }
@@ -32,6 +29,7 @@ public class AppTest {
     // STEP1. 로또 구매 시
     @Test
     public void 로또구매숫자값외의잘못된값입력() {
+        
         // 숫자값외의 잘못된 값입력
         String input = "qwer";
         OutputStream out = new ByteArrayOutputStream();
@@ -44,59 +42,59 @@ public class AppTest {
         assertEquals(expectErrorMessage, lottoService.buyLotto());
     }
 
-    @test
+    @Test
     public void 로또값보다작은값입력() {
         // TODO
     }
 
-    @test
+    @Test
     public void 로또값보다큰값입력() {
         // TODO : success
     }
 
     // STEP2. 수동 로또장수 입력
-    @test
+    @Test
     void 수동로또숫자값외의잘못된값입력() {
         // TODO
     }
 
-    @test
+    @Test
     public void 총로또장수보다큰값입력() {
         // TODO
     }
 
-    @test
+    @Test
     public void 마이너스값입력() {
         // TODO
     }
     
-    @test
+    @Test
     public void 총로또장수보다작지만영이상의값입력시() {
         // TODO : success
     }
 
     // STEP3. 수동 로또번호 입력
-    @test
+    @Test
     void 로또번호숫자값외의잘못된값입력() {
         // TODO
     }
     
-    @test
+    @Test
     void 영이하의값입력() {
         // TODO
     }
 
-    @test
+    @Test
     void 사십오초과값입력() {
         // TODO
     }
 
-    @test
+    @Test
     void 이미입력한로또번호재입력시() {
         // TODO
     }
 
-    @test
+    @Test
     void 여섯의중복되지않은숫자값입력시() {
         // TODO : success
     }
@@ -104,22 +102,22 @@ public class AppTest {
     // STEP4. 자동로또번호 출력
 
     // STEP5. 로또당첨번호 입력
-    @test
+    @Test
     void 당첨번호입력시숫자외의잚못된값입력() {
         // TODO
     }
 
-    @test
+    @Test
     void 영이하의값또는사십오초과의값입력() {
         // TODO
     }
 
-    @test
+    @Test
     void 이미입력한당첨번호중복입력시() {
         // TODO
     }
 
-    @test
+    @Test
     void 일곱의중복되지않은당첨번호입력시() {
         // TODO : success
     }
