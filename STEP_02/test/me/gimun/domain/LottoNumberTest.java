df39a9998d05b73b6f9fc00dfd26d58734b4a702
= <wjdrlans4000@naver.com>
2021-08-10 22:42:32 +0900

    refactor :  로또번호생성 테스트코드 수정
    잘못된 테스트 수정
    잘못된 로또번호 생성에 대한 테스트 추가

--- STEP_02/test/me/gimun/domain/LottoNumberTest.java
+++ STEP_02/test/me/gimun/domain/LottoNumberTest.java
@@ -14,15 +14,33 @@ class LottoNumberTest {
     void 로또번호객체생성() {
         List<LottoNumber> lotto = new ArrayList<>();
         int jj = 0;
+        //1,2,3,4,5,6
         while (jj != 6){
             lotto.add(LottoNumber.valueOf(jj));
             jj++;
         }
 
-        assertNotNull(lotto);
+        assertEquals(lotto.get(0).getNumber(),1);
+        assertEquals(lotto.get(1).getNumber(),2);
+        assertEquals(lotto.get(2).getNumber(),3);
+        assertEquals(lotto.get(3).getNumber(),4);
+        assertEquals(lotto.get(4).getNumber(),5);
+        assertEquals(lotto.get(5).getNumber(),6);
 
+        List<Integer> setLotto = new ArrayList<>();
         for (LottoNumber lottoNum : lotto) {
-            System.out.println(lottoNum.getNumber());
+            setLotto.add(lottoNum.getNumber());
         }
+
+        System.out.println(setLotto);
+    }
+
+    @Test
+    void 잘못된로또번호생성() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()-> new LottoNumber(0));
+        String message = exception.getMessage();
+
+        assertEquals("LottoNumber가 유효하지 않습니다.",message);
+
     }
 }
