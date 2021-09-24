e4deede838ef1ecb7c576b5d447638e324bd8a73
sjchoi <sjchoi@redsoft.co.kr>
2021-09-24 18:52:41 +0900

    # [FEATURE] 로또 구매

---
+++ STEP_02/lotto/src/main/java/com/bootcamp/lotto/model/dto/ChangeDto.java
@@ -0,0 +1,16 @@
+package com.bootcamp.lotto.model.dto;
+
+import lombok.Data;
+
+@Data
+public class ChangeDto { // 로또 구매에 따른 로또 갯수와 나머지 반환 Dto
+
+    private int lottoCount;
+    private int change;
+
+    public ChangeDto(int lottoCount, int change) {
+        this.lottoCount = lottoCount;
+        this.change = change;
+    }
+
+}
